39c535f269ccffc3562081c857409d41b2eaf970
EvgHrn <evg.hrn@gmail.com>
2016-09-13 15:49:44 +0300

    Added body-parser module

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 let app = express();
 let handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+let body;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
