bb81a7085485391b8c80d3c559b21e545aef70b1
EvgHrn <evg.hrn@gmail.com>
2016-09-20 17:33:48 +0400

    Added uno web-client sketch

--- WebClient/WebClient.ino
+++ WebClient/WebClient.ino
@@ -1,105 +1,71 @@
 /*
   Web client
- 
+
  This sketch connects to a website (http://www.google.com)
- using an Arduino Wiznet Ethernet shield. 
- 
+ using an Arduino Wiznet Ethernet shield.
+
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
- 
+
  created 18 Dec 2009
  by David A. Mellis
  modified 9 Apr 2012
  by Tom Igoe, based on work by Adrian McEwen
- modified 12 Aug 2013
- by Soohwan Kim
- Modified 18 Aug 2015
- by Vassilis Serasidis
- 
- =========================================================
- Ported to STM32F103 on 18 Aug 2015 by Vassilis Serasidis
 
- <---- Pinout ---->
- W5x00 <--> STM32F103
- SS    <-->  PA4 <-->  BOARD_SPI1_NSS_PIN
- SCK   <-->  PA5 <-->  BOARD_SPI1_SCK_PIN
- MISO  <-->  PA6 <-->  BOARD_SPI1_MISO_PIN
- MOSI  <-->  PA7 <-->  BOARD_SPI1_MOSI_PIN
- =========================================================
- 
- 
  */
 
 #include <SPI.h>
-#include <Ethernet_STM.h>
+#include <Ethernet.h>
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
-#if defined(WIZ550io_WITH_MACADDRESS) // Use assigned MAC address of WIZ550io
-;
-#else
-byte mac[] = {0xDE, 0xAC, 0xBE, 0xEF, 0xFE, 0xED};
-#endif  
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-//IPAddress server(5,63,159,247);  // numeric IP for Serasidis.gr (no DNS)
-IPAddress server(5,63,159,247);    // name address for Serasidis.gr (using DNS)
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "www.google.com";    // name address for Google (using DNS)
 
 // Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192,168,1,177);
+IPAddress ip(192, 168, 0, 177);
 
 // Initialize the Ethernet client library
-// with the IP address and port of the server 
+// with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
 void setup() {
- // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  // while (!Serial) {
-   // ; // wait for serial port to connect. Needed for Leonardo only
- // }
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
   // start the Ethernet connection:
-#if defined(WIZ550io_WITH_MACADDRESS)
-  if (Ethernet.begin() == 0) {
-#else
   if (Ethernet.begin(mac) == 0) {
-#endif  
     Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
     // try to congifure using IP address instead of DHCP:
-#if defined(WIZ550io_WITH_MACADDRESS)
-    Ethernet.begin(ip);
-#else
     Ethernet.begin(mac, ip);
-#endif  
   }
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
-  String PostData = "id=667&plotter=4&startTime=35&stopTime=345&passes=45&meters=45";
+
   // if you get a connection, report back via serial:
-  if (client.connect(server, 3001)) {
+  if (client.connect(server, 80)) {
     Serial.println("connected");
-    // Make an HTTP request:
-    client.println("POST /quotes HTTP/1.1");
-    client.println("Host: 5.63.159.247:3001");
-    client.println("Cache-Control: no-cache");
-    client.println("Postman-Token: eab61e60-f3ca-abc0-e506-07a925ed1ebb");
-    client.println("Content-Type: application/x-www-form-urlencoded");
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
     client.println();
-    client.println(PostData);
-  } 
-  else {
+  } else {
     // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
 }
 
-void loop()
-{
-  // if there are incoming bytes available 
+void loop() {
+  // if there are incoming bytes available
   // from the server, read them and print them:
   if (client.available()) {
     char c = client.read();
@@ -113,7 +79,7 @@ void loop()
     client.stop();
 
     // do nothing forevermore:
-    while(true);
+    while (true);
   }
 }
 
